1ea892abf2aebfab014241ed79ce691651feadcc
227felix <130098040+227felix@users.noreply.github.com>
2023-10-10 21:17:15 +0200

    layed the foundation for the flask app

--- epanaFlask/app.py
+++ epanaFlask/app.py
@@ -1,13 +1,24 @@
 from flask import Flask, flash, redirect, render_template, request, session
+from helpers import login_required
 
 app = Flask(__name__)
 
-#db =
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+#db = SQLAlchemy(app)
+
+
+@app.route('/', methods=["GET", "POST"])
+# @login_required
+def index():  # put application's code here
+    if request.method == "POST":
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
