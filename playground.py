84078f9d2143789798e00d28210352f8caba5853
227felix <felix.grenzing@studium.uni-hamburg.de>
2023-10-10 16:09:09 +0200

    start the conversation with the system prompt

--- playground.py
+++ playground.py
@@ -6,6 +6,25 @@ def start_convo(api_key, model_id):
     openai.api_key = api_key
     print("Enter 's' in chat to stop conversation\n")
 
+    # Start the conversation with the system prompt
+    response = openai.ChatCompletion.create(
+        model=model_id,
+        messages=[
+            {
+                "role": "user",
+                "content": "Du bist mein guter Freund Jesper mit dem ich kurze Textkonversationen über Gott und die Welt führe."
+            }
+        ],
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Provide user with response of chatbot
+    print("Assistant: " + response.choices[0].message.content)
+
     while True:
         # Get input prompt of user
         user_prompt: str = input("User: ")
