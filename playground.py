20570aad70b2e13d274ed3f619394004904029df
227felix <130098040+227felix@users.noreply.github.com>
2023-10-10 17:05:31 +0200

    fixed CONVO

--- playground.py
+++ playground.py
@@ -4,6 +4,7 @@ import openai
 CONVO = []
 SYSTEM_PROMPT = "Du bist mein guter Freund Jesper mit dem ich kurze Textkonversationen über Gott und die Welt führe."
 
+
 def start_convo(api_key, model_id):
     openai.api_key = api_key
     print("Enter 's' in chat to stop conversation\n")
@@ -25,6 +26,10 @@ def start_convo(api_key, model_id):
     )
 
     # Provide user with response of chatbot
+    CONVO.append({
+        "role": "user",
+        "content": SYSTEM_PROMPT
+    })
     CONVO.append(response.choices[0].message)
     print("Assistant: " + response.choices[0].message.content)
 
@@ -36,10 +41,15 @@ def start_convo(api_key, model_id):
         if len(CONVO) > 20:
             CONVO.pop(1)
 
+        # Add user prompt to conversation
+        CONVO.append({
+            "role": "user",
+            "content": user_prompt
+        })
         # Generate response based on prompt
         response = openai.ChatCompletion.create(
             model=model_id,
-            messages = CONVO,
+            messages=CONVO,
             temperature=1,
             max_tokens=256,
             top_p=1,
