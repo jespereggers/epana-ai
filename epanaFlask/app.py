1e288afaf8a5a7c4c9afcaf549d5e681b24c6220
227felix <130098040+227felix@users.noreply.github.com>
2023-10-12 18:25:28 +0200

    added/improved file upload and processing; improved the DB

--- epanaFlask/app.py
+++ epanaFlask/app.py
@@ -1,3 +1,7 @@
+# naming convention for saved files: output_<file_id>.jsonl for the output file,
+# verification_<file_id>.jsonl for the verification file and upload_<file_id>.txt for the uploaded file
+import datetime
+
 from flask import Flask, flash, redirect, render_template, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -46,26 +50,88 @@ def models():
     else:
         db = get_db()
         cursor = db.cursor()
-        cursor.execute("SELECT model_id FROM models WHERE owner_id = ?", (session["user_id"],))
+        # TODO: model database should probably hold the original filename to make it easier to identify the model
+        cursor.execute("SELECT model_id FROM models WHERE owner_id = (?)", (session["user_id"],))
         model_info = cursor.fetchall()
-        return render_template("models.html", models=model_info)
+        cursor.execute("SELECT name, date FROM input_files WHERE owner_id = (?)", (session["user_id"],))
+        file_info = cursor.fetchall()
+        # formate date to dd-mm-yyyy hh:mm
+        formatted_file_info = [
+            (file, datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S').strftime('%d-%m-%Y %H:%M')) for file, date
+            in file_info]
+        return render_template("models.html", models=model_info, files=formatted_file_info)
 
 
 @app.route('/create_model', methods=["GET", "POST"])
 @login_required
 def create_model():
     if request.method == "POST":
-        if not request.files['file']:
-            return apology("Please input a file", 400)
-        file = request.files['file']
-        filename = "placeholder.txt"
-        filepath = "file_uploads/" + filename
-        file.save(filepath)
-        chat_to_jsonl(filepath, "output_files/output.jsonl", "output_files/verification.jsonl")
-        
         return render_template("create_model.html")
     else:
         return render_template("create_model.html")
+
+
+@app.route('/upload_file', methods=["GET", "POST"])
+@login_required
+def upload_file():
+    if request.method == "POST":
+
+        # FIXME: resubmitting the form will create a duplicate file; not sure how to fix; this might help:
+        #  https://en.m.wikipedia.org/wiki/Post/Redirect/Get
+        # connect to database
+        db = get_db()
+        cursor = db.cursor()
+
+        # check if file was uploaded
+        if not request.files['file']:
+            return apology("Please input a file", 400)
+
+        file = request.files['file']
+        # craft filename
+        cursor.execute("SELECT MAX(id) FROM input_files")
+        max_input_id = cursor.fetchall()[0][0]
+        if max_input_id is None:
+            max_input_id = -1
+        input_filename = "upload_" + str(max_input_id + 1) + ".txt"
+        original_filename = file.filename
+        # check if there is a row with the same filename in the database
+        cursor.execute("SELECT * FROM input_files WHERE name LIKE ?", (original_filename + "%",))
+        rows = cursor.fetchall()
+        if len(rows) > 0:
+            input_filename_db = original_filename + str(len(rows))
+            print(str(len(rows)))
+        else:
+            input_filename_db = original_filename
+
+        # save file to server
+        filepath = "file_uploads/" + input_filename
+        file.save(filepath)
+        # add file to database
+        cursor.execute("INSERT INTO input_files (owner_id, name) VALUES (?, ?)",
+                       (session["user_id"], input_filename_db))
+
+        # FIXME: make this more robust (e.g. make the output file name dependent on the amount of output files not
+        #  input files in the database)
+        # craft output filename
+        output_name = "output_" + str(max_input_id + 1) + ".jsonl"
+        output_path = "output_files/" + output_name
+        verification_name = "verification_" + str(max_input_id + 1) + ".jsonl"
+        verification_path = "output_files/" + verification_name
+        # convert file to jsonl
+        chat_to_jsonl(filepath, output_path, verification_path)
+        # add files to database
+        # TODO: add original filename to database instead of the generated one (DONE) or probably just delete the name
+        #  column from the database cuz its not needed but idk, db structure might need some improvements
+        cursor.execute("INSERT INTO output_files (owner_id, type, name) VALUES (?, ?, ?)",
+                       (session["user_id"], "output", input_filename_db + "output"))
+        cursor.execute("INSERT INTO output_files (owner_id, type, name) VALUES (?, ?, ?)",
+                       (session["user_id"], "verification", input_filename_db + "verification"))
+        flash("File: " + original_filename + " uploaded successfully", "success")
+        db.commit()
+
+        return redirect("/models")
+    else:
+        return render_template("upload_file.html")
 
 
 @app.route('/account', methods=["GET", "POST"])
@@ -147,14 +213,12 @@ def register():
                 (email, generate_password_hash(password)),
             )
             db.commit()
-            print("User created")
         except sqlite3.IntegrityError:
             return apology("email already in use", 400)
 
         cursor.execute("SELECT id FROM users WHERE email = ?", (email,))
         rows = cursor.fetchall()
         user_id = rows[0][0]
-        print(user_id)
         session["user_id"] = user_id
 
         return redirect("/")
