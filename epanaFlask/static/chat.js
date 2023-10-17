59f15381c0a9e96ef9b67f8a3bcda23033238c35
227felix <130098040+227felix@users.noreply.github.com>
2023-10-17 18:35:57 +0200

    improved the chat function

--- epanaFlask/static/chat.js
+++ epanaFlask/static/chat.js
@@ -1,6 +1,27 @@
 const USER = true;
 const ASSISTANT = false;
 
+document.addEventListener("DOMContentLoaded", function () {
+    const inputField = document.getElementById("chat-input");
+    const button = document.getElementById("chat-button");
+
+    // Add an event listener to the input field
+    inputField.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            // Prevent the form from submitting (if inside a form)
+            event.preventDefault();
+
+            // Trigger the button's click event
+            button.click();
+        }
+    });
+
+    // Handle button click
+    button.addEventListener("click", function () {
+        getChat(); // Call the getChat() function when the button is clicked
+    });
+});
+
 function addNewChatbox(text, player) {
     let chatbox = document.createElement("div");
     chatbox.className = "chatbox";
@@ -16,6 +37,8 @@ function addNewChatbox(text, player) {
 
 function getChat() {
     let message = document.getElementById("chat-input").value;
+    document.getElementById("chat-input").value = "";
+    document.getElementById("chat-input").focus();
     addNewChatbox(message, USER)
 
 
